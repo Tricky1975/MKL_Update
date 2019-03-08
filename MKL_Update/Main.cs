9b8ae6579a3c87b9703da0895040a07ca799503c
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-08 11:36:58 +0100

    Links to dependencies

--- MKL_Update/Main.cs
+++ MKL_Update/Main.cs
@@ -1,13 +1,34 @@
 using System;
 using System.Collections.Generic;
+using TrickyUnits;
 
 namespace MKL_Update
 {
     class MKL_Main
     {
+        static FlagParse MyArgs;
+
+        private static void InitArgs() {
+            MyArgs.CrBool("version", false);
+            MyArgs.CrBool("help", false);
+            MyArgs.CrBool("h", false);
+            if (!MyArgs.Parse(true)) throw new Exception("Invalid input");
+            if (MyArgs.GetBool("version")) {
+                Console.WriteLine(MKL.All(true));
+                Environment.Exit(0);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Start up template");
+            MKL.Lic("", "");
+            MKL.Version("", "10.10.10");
+            MyArgs = new FlagParse(args);
+            InitArgs();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("MaKe License - Update");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"Version: {MKL.Newest}");
             Console.ReadKey();
         }
     }
