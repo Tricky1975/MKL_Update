8be42f4f07bedcfdebb5e55b7fc470a6d9820fa1
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-08 01:11:17 +0100

    1st Template - Code comes later

---
+++ MKL_Update/Main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKL_Update
+{
+    class MKL_Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start up tempalte");
+            Console.ReadKey();
+
+        }
+    }
+}
