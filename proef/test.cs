fccdaa8da6a78861a99d44b9f7666fb0d562b9e4
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-09 11:44:24 +0100

    Test files after being modified by MKL_Update

--- proef/test.cs
+++ proef/test.cs
@@ -1,7 +1,32 @@
+// Lic:
+// test.cs
+// version: 19.03.09
+// set up in years: 
+// 
+// This file is simply a collection of functions and featuers other people wrote
+// And all I did was collect them. I therefore cannot license this file itself.
+// Use it as you see fit.
+// 
+// Please respect the original authors of the contents and don't claim ownership
+// on them or sublicense this in any way, unless you have the original author's
+// explicit permission to do so.
+// 
+// Wherever possible I credited the original author of the content.
+// EndLic
+
+
+
 class Testme{
 
+
+
 static void Main(){
+
 System.Console.WriteLine("Hello World");
-}
 
 }
+
+
+
+}
+
--- proef/test.py
+++ proef/test.py
@@ -1,1 +1,15 @@
+"""   -- Start License block
+test.py
+(c)  Always look on the bright side of life.
+
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0. If a copy of the MPL was not
+distributed with this file, You can obtain one at
+http://mozilla.org/MPL/2.0/.
+Version: 19.03.09
+-- End License block   """
+
+
+
 print "Hello World"
+
